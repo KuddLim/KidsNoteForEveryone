d8f859bbbb6832453cb7e81acdc2da934940a814
Intaek Lim <masterhand@gmail.com>
2020-05-27 08:54:54 +0900

    누락파일 추가

---
+++ Libs/LibKidsNoteForEveryone/EncryptorAES.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LibKidsNoteForEveryone
+{
+    public class EncryptorAES
+    {
+        public static byte[] DefaultAesEncKey = Encoding.UTF8.GetBytes("KiDs_nOTe_BoT!!!");   // 변경해서 사용하는 것을 권장
+        private static byte[] DefaultAesEncIV = Encoding.UTF8.GetBytes("~!@#$%^&*()_+|}{");    // 변경해서 사용하는 것을 권장
+
+        static private AesCryptoServiceProvider CreateAesProvider(byte[] key)
+        {
+            return new AesCryptoServiceProvider
+            {
+                KeySize = 256,
+                BlockSize = 128,
+                Key = key,
+                IV = DefaultAesEncIV,
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC
+            };
+        }
+
+        static public string EncryptAes(string plain, byte[] key)
+        {
+            byte[] source = System.Text.Encoding.UTF8.GetBytes(plain);
+            AesCryptoServiceProvider provider = CreateAesProvider(key);
+            ICryptoTransform encryptor = provider.CreateEncryptor();
+
+            byte[] encryptedBytes = encryptor.TransformFinalBlock(source, 0, source.Length);
+            string encrypted = Convert.ToBase64String(encryptedBytes);
+            return encrypted;
+        }
+
+        static public string DecryptAes(string encrypted, byte[] key)
+        {
+            byte[] source = Convert.FromBase64String(encrypted);
+            AesCryptoServiceProvider provider = CreateAesProvider(key);
+            ICryptoTransform encryptor = provider.CreateDecryptor();
+
+            byte[] decryptedBytes = encryptor.TransformFinalBlock(source, 0, source.Length);
+            string decrypted = System.Text.Encoding.UTF8.GetString(decryptedBytes);
+            return decrypted;
+        }
+
+        public void EncryptChaCha(FileStream input, FileStream output, byte[] key)
+        {
+            //ParametersWithIV parameters = new ParametersWithIV(new KeyParameter(DefaultChaChaEncKey), DefaultChaChaEncNonce);
+            //ChaChaEngine engine = new ChaChaEngine(20);
+            //engine.Init(true, parameters);
+            //engine.ProcessBytes(FullPacket, 0, FullPacket.Length, outData, 8);
+            //Buffer.BlockCopy(nonce, 0, outData, 0, 8);
+        }
+    }
+}
---
+++ Libs/LibKidsNoteForEveryone/EncryptorChaCha.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Bcpg;
+using Org.BouncyCastle.Crypto.Engines;
+
+namespace LibKidsNoteForEveryone
+{
+    public class EncryptorChaCha
+    {
+        public static byte[] DefaultChaChaEncKey = Encoding.UTF8.GetBytes("나의차차기본키입니다^^");//("`~1!2@3#4$5%6^7&8*9(0)-_=+]}[{/?");
+        public static byte[] DefaultChaChaEncNonce = Encoding.UTF8.GetBytes("19530124");
+
+        private bool Encrypting;
+        public byte[] Key { get; }
+        public byte[] Nonce { get; }
+        private ChaChaEngine Engine;
+
+        public EncryptorChaCha(bool encrypting, byte[] key, byte[] nonce)
+        {
+            Encrypting = encrypting;
+            Key = key;
+            Nonce = nonce;
+
+            ParametersWithIV parameters = new ParametersWithIV(new KeyParameter(DefaultChaChaEncKey), DefaultChaChaEncNonce);
+
+            Engine = new ChaChaEngine(20);
+            Engine.Init(encrypting, parameters);
+        }
+
+        public void Process(byte[] input, int srcPos, int srcLen, byte[] output, int outPos)
+        {
+            Engine.ProcessBytes(input, srcPos, srcLen, output, outPos);
+        }
+    }
+}
